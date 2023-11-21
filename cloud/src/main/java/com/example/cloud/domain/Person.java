02085840c1d4da110547602949171816764351ee
Keti Gugulashvili <gugulashviliketi@gmail.com>
2023-11-21 16:34:53 +0400

    modify logstash and versions

--- cloud/src/main/java/com/example/cloud/domain/Person.java
+++ cloud/src/main/java/com/example/cloud/domain/Person.java
@@ -1,14 +1,12 @@
 package com.example.cloud.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 public class Person {
     private int id;
     private String firstName;
--- cloud/src/test/java/com/example/cloud/CloudApplicationTests.java
+++ cloud/src/test/java/com/example/cloud/CloudApplicationTests.java
@@ -1,13 +1,48 @@
 package com.example.cloud;
 
+import com.example.cloud.domain.Person;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CloudApplicationTests {
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
-	void contextLoads() {
+	@Order(1)
+	public void testAdd() {
+		Person person = new Person(1, "keti", "gugulashvili", 24);
+		HttpEntity<Person>entity = new HttpEntity<>(person);
+		var response = restTemplate.exchange("/persons", HttpMethod.POST, entity, Person.class);
+		assertEquals(200, response.getStatusCode().value());
+		assertEquals(person, response.getBody());
+	}
+
+	@Test
+	@Order(2)
+	public void testFindById() {
+		Person person = restTemplate.getForObject("/persons/{id}", Person.class, 1);
+		assertNotNull(person);
+		assertEquals(1, person.getId());
+	}
+
+	@Test
+	@Order(3)
+	public void testPut() {
+		Person person = new Person(1, "John", "Smith", 30);
+		HttpEntity<Person> entity = new HttpEntity<>(person);
+		var response = restTemplate.exchange("/persons", HttpMethod.PUT, entity, Person.class);
+		assertEquals(200, response.getStatusCode().value());
+		assertEquals(person, response.getBody());
 	}
 
 }
